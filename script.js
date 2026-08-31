9b1429fe0ec66a884b9f5fd3fb65cf0c9e383831
krilph <krealf@mail.ru>
2026-08-31 20:45:23 +0300

    Заменил p на тэг span в skills__item, добавил фильтрацию по категории и поисковому полю.

--- script.js
+++ script.js
@@ -1,31 +1,67 @@
+let currentCategory = document.querySelector('input[name="category"]:checked').value
+let currentSearch = document.querySelector('#search').value
+
 const toggleThemeButton = document.getElementById("btn-toggle-theme")
+const inputsCategory = document.querySelectorAll('input[name="category"]')
+const searchInput = document.querySelector("#search")
+const skillsItems = document.querySelectorAll('.skills__item')
 const html = document.documentElement
 
+// Функция получения текущей темы из data-атрибута или предпочитаемой темы от браузера
 const getTheme = () => {
     return html.getAttribute("data-theme") || (window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light")
 }
 
+// Функция установки темы в data-атрибут, в localstorage и изменение текста кнопки смены темы
 const setTheme = (theme) => {
     html.setAttribute('data-theme', theme)
     localStorage.setItem("theme", theme)
     updateToggleText(theme)
 }
 
+// Функция смены текста у кнопки смены темы
 const updateToggleText = (theme) => {
     const textButton = toggleThemeButton.querySelector('.toggle-text')
     textButton.textContent = theme === "dark" ? "Dark" : "Light"
     textButton.setAttribute("aria-label", `Переключить на ${theme === "dark" ? "светлую" : "тёмную"} тему`)
 }
 
-updateToggleText(getTheme())
-
-toggleThemeButton.addEventListener('click', () => {
-    const current = getTheme();
-    setTheme(current === "dark" ? "light" : "dark")
-})
-
+// При изменении предпочитаемой схемы от браузера и отсутствии в localstorage темы, происходит смена темы
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener("change", (e) => {
     if (!localStorage.getItem('theme')) {
         setTheme(e.matches ? 'dark' : "light")
     }
 })
+
+// Функция фильтрации по категории и поиску
+const filterAndSearch = () => {
+    skillsItems.forEach((skill) => {
+        const textSpan = skill.querySelector("span")
+
+        if (skill.getAttribute('data-category') === currentCategory && textSpan.textContent.toLowerCase().includes(currentSearch.toLowerCase())) {
+            skill.style.display = 'flex'
+        } else if (currentCategory === 'all' && textSpan.textContent.toLowerCase().includes(currentSearch.toLowerCase())) {
+            skill.style.display = 'flex'
+        } else {
+            skill.style.display = 'none'
+        }
+    })
+}
+
+// Обработчик на поле поиска
+searchInput.addEventListener('input', (event) => {
+    currentSearch = event.target.value
+    filterAndSearch()
+})
+
+// Обработчики на поля input на смену категории
+inputsCategory.forEach((input) => {
+    input.addEventListener('change', (event) => {
+        currentCategory = event.target.value
+        filterAndSearch()
+    })
+})
+
+
+// Смена текста у кнопки при инициализации
+updateToggleText(getTheme())
